2dca21ae0fd1344d0405443bb30e5b0ea7b712db
fish <>
2025-12-19 16:59:28 +0800

    新增AppHeader與UnitButton元件與story檔案

---
+++ src/component/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-container">
+    <img src="@/assets/images/logo.svg" alt="nothing">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  type: {
+    type: String,
+    default: 'primary',
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.header-container {
+  width: 100%;
+  height: 100px;
+}
+</style>
