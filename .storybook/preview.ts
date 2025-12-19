2f2ec42a608e8d32ed2231fbbeb9b5ce39e4cb1a
fish <>
2025-12-19 17:01:54 +0800

    新增global.scss檔案，並新增storybook自己的tsconfig，幫助路徑解析

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/vue3-vite'
+import '@/styles/global.scss'
 
 const preview: Preview = {
   parameters: {
@@ -8,13 +9,20 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
       test: 'todo',
     },
+    // backgrounds: {
+    //   default: 'dark',
+    //   values: [
+    //     { name: 'light', value: '#ffffff' },
+    //     { name: 'dark', value: '#03002C' },
+    //     { name: 'black', value: '#000000' },
+    //   ],
+    // },
   },
 }
 
